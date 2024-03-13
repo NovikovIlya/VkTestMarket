44f9b5fe1264fa192330e90f61dc358592392cd1
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-03-13 10:39:57 +0300

    21

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import Basket from "./pages/Basket/Basket.tsx";
 import "./App.css";
 
 function App() {
   return (
     <>
-      
-        <Basket />
-     
+      <Basket />
     </>
   );
 }
